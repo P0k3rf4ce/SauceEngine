76eab63e7e0e6887c2df7416de2f509a68d6364f
su-ah <emmrald4@gmail.com>
2025-09-26 23:52:58 -0400

    shader file cleanup

--- src/rendering/shaders/pbr/pbr.fs
+++ src/rendering/shaders/pbr/pbr.fs
@@ -5,12 +5,13 @@ in vec3 WorldPos;
 in vec3 Normal;
 
 // material parameters
-uniform vec3 albedo;
-uniform float metallic;
-uniform float roughness;
-uniform float ao;
+uniform sampler2D albedoMap;
+uniform sampler2D normalMap;
+uniform sampler2D metallicMap;
+uniform sampler2D roughnessMap;
+uniform sampler2D aoMap;
 
-// ibl
+// IBL
 uniform samplerCube irradianceMap;
 uniform samplerCube prefilterMap;
 uniform sampler2D brdfLUT;
@@ -22,6 +23,24 @@ uniform vec3 lightColors[4];
 uniform vec3 camPos;
 
 const float PI = 3.14159265359;
+
+// trick to get tangent normals to world space - according to book source code
+vec3 getNormal()
+{
+    vec3 tangentNormal = texture(normalMap, TexCoords).xyz * 2.0 - 1.0;
+
+    vec3 Q1  = dFdx(WorldPos);
+    vec3 Q2  = dFdy(WorldPos);
+    vec2 st1 = dFdx(TexCoords);
+    vec2 st2 = dFdy(TexCoords);
+
+    vec3 N   = normalize(Normal);
+    vec3 T   = normalize(Q1*st2.t - Q2*st1.t);
+    vec3 B   = -normalize(cross(N, T));
+    mat3 TBN = mat3(T, B, N);
+
+    return normalize(TBN * tangentNormal);
+}
 
 // distribution approx
 float distributionGGX(vec3 N, vec3 H, float roughness)
@@ -59,11 +78,16 @@ vec3 fresnelSchlick(float cosTheta, vec3 F0)
 vec3 fresnelSchlickRoughness(float cosTheta, vec3 F0, float roughness)
 {
     return F0 + (max(vec3(1.0 - roughness), F0) - F0) * pow(clamp(1.0 - cosTheta, 0.0, 1.0), 5.0);
-}   
+}
 
 void main()
 {		
-    vec3 N = Normal;
+    vec3 albedo     = pow(texture(albedoMap, TexCoords).rgb, vec3(2.2));
+    float metallic  = texture(metallicMap, TexCoords).r;
+    float roughness = texture(roughnessMap, TexCoords).r;
+    float ao        = texture(aoMap, TexCoords).r;
+
+    vec3 N = getNormal(); // function from book
     vec3 V = normalize(camPos - WorldPos);
     vec3 R = reflect(-V, N);
 
@@ -86,34 +110,34 @@ void main()
         // calculate brdf
         float D   = distributionGGX(N, H, roughness);   
         float G   = geometrySmith(N, V, L, roughness);      
-        vec3 F    = fresnelSchlick(max(dot(H, V), 0.0), F0);
+        vec3 F    = fresnelSchlick(clamp(dot(H, V), 0.0, 1.0), F0);
            
         vec3 num      = D * G * F; 
         float denom   = 4.0 * max(dot(N, V), 0.0) * max(dot(N, L), 0.0);
         vec3 specular = num / max(denom, 0.001); // prevent divide by zero
         
-        vec3 kd = vec3(1.0) - F;    // conservation of energy
-        kd *= 1.0 - metallic;       // only non-metals have diffuse lighting; linearly blend if partly metal
+        vec3 kd = vec3(1.0) - F; // conservation of energy
+        kd *= 1.0 - metallic;	  
 
         Lo += (kd * albedo / PI + specular) * radiance * max(dot(N, L), 0.0);
     }   
     
-    // ambient lighting - we now use IBL
+    // ambient lighting
     vec3 F = fresnelSchlickRoughness(max(dot(N, V), 0.0), F0, roughness);
     vec3 kd = 1.0 - F;
     kd *= 1.0 - metallic;
 
-    vec3 diffuse = texture(irradianceMap, N).rgb * albedo;
+    vec3 irradiance = texture(irradianceMap, N).rgb;
+    vec3 diffuse    = irradiance * albedo;
 
     // sample pre-filter map and lut, then combine via split-sum approx
     const float MAX_REFLECTION_LOD = 4.0;
-    vec3 prefilteredColor = textureLod(prefilterMap, R, roughness * MAX_REFLECTION_LOD).rgb;
+    vec3 prefilteredColor = textureLod(prefilterMap, R,  roughness * MAX_REFLECTION_LOD).rgb;    
     vec2 brdf             = texture(brdfLUT, vec2(max(dot(N, V), 0.0), roughness)).rg;
     vec3 specular         = prefilteredColor * (F * brdf.x + brdf.y);
 
     vec3 ambient = (kd * diffuse + specular) * ao;
-
-    vec3 color = ambient + Lo;
+    vec3 color   = ambient + Lo;
 
     color = color / (color + vec3(1.0));    // hdr tonemapping
     color = pow(color, vec3(1.0/2.2));      // gamma correct
