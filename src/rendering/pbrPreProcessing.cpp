ab8ccb39b5aadbfb7a80090589ce0f0e539091f3
su-ah <emmrald4@gmail.com>
2025-09-30 18:41:40 -0400

    fix: forgot a return statment

--- src/rendering/pbrPreProcessing.cpp
+++ src/rendering/pbrPreProcessing.cpp
@@ -139,6 +139,8 @@ setupPrefilterMap(const int size)
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glGenerateMipmap(GL_TEXTURE_CUBE_MAP);
+
+    return prefilterMap;
 }
 
 /**
