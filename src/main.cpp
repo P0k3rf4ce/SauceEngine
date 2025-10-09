f3f58f874f6ca2e80e3ec30c4ad913aabb9a70b5
Ethan Vince-Budan <ethan.vince.budan@gmail.com>
2025-10-08 23:01:20 -0400

    Jarvis, revert that change

--- src/main.cpp
+++ src/main.cpp
@@ -49,7 +49,7 @@ int main()
 
 void initGLFW() {
     glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
