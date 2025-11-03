770d25e3aa3694d1f2b1b8bf40b88922aa6c5bdc
Tymofiy Sompura <77696830+P0k3rf4ce@users.noreply.github.com>
2025-11-02 23:42:05 -0500

    Issue 106 - Create a Launcher (#124)
    * Implement option parsing
    
    * Fix warning in optionParser tests
    
    * Remove unistd.h import
    
    * Add QT widgets
    
    * Add a basic Launcher
    
    * Add skip-launcher option and move fn def for engineMainloop
    
    ---------
    
    Co-authored-by: Tymofiy Sompura <sompurat@mail.utoronto.ca>

--- include/launcher/optionParser.hpp
+++ include/launcher/optionParser.hpp
@@ -10,16 +10,19 @@
 #include <boost/program_options/parsers.hpp>
 
 struct AppOptions {
-    const unsigned int DEFAULT_SCR_WIDTH = 800;
-    const unsigned int DEFAULT_SCR_HEIGHT = 600;
-    const double DEFAULT_TICKRATE = 128.0;
+    static constexpr unsigned int DEFAULT_SCR_WIDTH = 800;
+    static constexpr unsigned int DEFAULT_SCR_HEIGHT = 600;
+    static constexpr double DEFAULT_TICKRATE = 128.0;
 
     unsigned int scr_width, scr_height;
     double tickrate;
     std::string scene_file;
     bool help;
+    bool skip_launcher;
 
     AppOptions(int argc, const char *argv[]);
+    AppOptions(): scr_width(DEFAULT_SCR_WIDTH), scr_height(DEFAULT_SCR_HEIGHT), tickrate(DEFAULT_TICKRATE), scene_file(), help(false) {}
+
     boost::program_options::options_description getHelpMessage() const;
 
 private:
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,12 @@
-#define STB_IMAGE_IMPLEMENTATION
-
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-#include <stb_image.h>
+#include <QApplication>
 
 #include <chrono>
 
 #include <iostream>
 
 #include "launcher/optionParser.hpp"
-#include "shared/Scene.hpp"
-
-void initGLFW();
-GLFWwindow* initWindow(unsigned int scr_width, unsigned int scr_height);
-bool initGLAD();
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void processInput(GLFWwindow* window);
-
-// Precision should be up to a millisecond
-inline double get_seconds_since_epoch();
+#include "launcher/launcherWindow.hpp"
+#include "launcher/engineMainloop.hpp"
 
 int main(int argc, const char *argv[]) {
     const AppOptions ops(argc, argv);
@@ -26,90 +14,16 @@ int main(int argc, const char *argv[]) {
     if (ops.help) {
         std::cout << "Usage: " << argv[0] << " <options> [scene_file]" << std::endl;
         std::cout << ops.getHelpMessage() << std::endl;
-        exit(1);
-    }
-
-    initGLFW();
-    GLFWwindow *window = initWindow(ops.scr_width, ops.scr_height);
-    if (window == NULL) {
         return 1;
     }
 
-    if (!initGLAD()) {
-        std::cerr << "Failed to load GLAD" << std::endl;
-        glfwTerminate();
-        return 1;
+    if (ops.skip_launcher) {
+        std::cout << "Skipping Launcher ..." << std::endl;
+        return engine_mainloop(ops);
     }
 
-    Scene scene;
-
-    double deltatime = 0.0;
-
-    double prev_frame_time = get_seconds_since_epoch(), current_frame_time;
-
-    const double delta_step = 1.0/ops.tickrate;
-
-    while (!glfwWindowShouldClose(window)) {
-        processInput(window);
-
-        current_frame_time = get_seconds_since_epoch();
-        deltatime += current_frame_time - prev_frame_time;
-        prev_frame_time = current_frame_time;
-
-        deltatime = scene.update(deltatime, delta_step);
-        
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
-    glfwTerminate();
-    return 0;
-}
-
-void initGLFW() {
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-#ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
-}
-
-GLFWwindow *initWindow(unsigned int scr_width, unsigned int scr_height) {
-    GLFWwindow* window = glfwCreateWindow(scr_width, scr_height, "Sauce Engine", NULL, NULL);
-    if (window == NULL)
-    {
-        std::cerr << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return NULL;
-    }
-    glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-    return window;
-}
-
-bool initGLAD() {
-    return gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
-}
-
-// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
-void processInput(GLFWwindow* window)
-{
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
-}
-
-// glfw: whenever the window size changed (by OS or user resize) this callback function executes
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-    glViewport(0, 0, width, height);
-}
-
-// Precision should be up to a millisecond
-inline double get_seconds_since_epoch() {
-    auto now = std::chrono::system_clock::now();
-    return std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()).count() / 1000.0;
+    QApplication app(argc, const_cast<char**>(argv));
+    LauncherWindow window(argc, argv);
+    window.show();
+    return app.exec();
 }
