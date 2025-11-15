b681bf27693ee1c1fe25a257848b0fda02b3433d
Ziad Zananiri <ziadzananirii@gmail.com>
2025-11-15 14:00:13 -0500

    Updated scene class

--- include/shared/Scene.hpp
+++ include/shared/Scene.hpp
@@ -3,16 +3,28 @@
 
 #include <string>
 #include <vector>
+#include <memory>
 
 #include "shared/Object.hpp"
+#include "rendering/LightProperties.hpp"
 
 class Scene {
 private:
     std::vector<Object> objects;
+
+    // store lights as polymorphic objects (shared ownership)
+    std::vector<std::shared_ptr<rendering::LightProperties>> lights;
+
+    // pointer to the currently active scene
+    static Scene *s_activeScene;
 public:
     Scene();
     Scene(std::string &filename);
     ~Scene();
+
+    // getters
+    const std::vector<std::shared_ptr<rendering::LightProperties>> &getLights() const noexcept;
+    static Scene *getActiveScene() noexcept;
 
     void load();
     void unload();
--- src/shared/Scene.cpp
+++ src/shared/Scene.cpp
@@ -1,15 +1,21 @@
 #include "shared/Scene.hpp"
 
+// define static active scene pointer
+Scene *Scene::s_activeScene = nullptr;
+
 Scene::Scene() {
-    
+    // set this scene as the active scene
+    s_activeScene = this;
 }
 
 Scene::Scene(std::string &filename) {
-    
+    s_activeScene = this;
 }
 
 Scene::~Scene() {
-
+    if (s_activeScene == this) {
+        s_activeScene = nullptr;
+    }
 }
 
 /**
@@ -38,4 +44,12 @@ void Scene::update(double timestep) {
     for (auto object: this->objects) {
         object.update(timestep);
     }
+}
+
+const std::vector<std::shared_ptr<rendering::LightProperties>> &Scene::getLights() const noexcept {
+    return lights;
+}
+
+Scene *Scene::getActiveScene() noexcept {
+    return s_activeScene;
 }
