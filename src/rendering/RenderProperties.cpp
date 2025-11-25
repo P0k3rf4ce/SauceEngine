55519cdf874a18e3b0ecd5641008df078a820892
su-ah <emmrald4@gmail.com>
2025-11-25 01:04:13 -0500

    object drawing

--- src/rendering/RenderProperties.cpp
+++ src/rendering/RenderProperties.cpp
@@ -22,5 +22,18 @@ void RenderProperties::unload() {
  * Run shaders for this object
 */
 void RenderProperties::update(const modeling::ModelProperties &modelProps, const animation::AnimationProperties &animProps) {
+
+    // set pbr stuff - currently not a thing
+    // set shadow stuff - currently not a thing
+
+    // set model matrix
+    // set projection matrix(?)
+    // set textures
     
+    // draw each mesh
+    std::vector<std::shared_ptr<Mesh>> meshes = modelProps.getMeshes;
+    for (const std::shared_ptr<Mesh>& mesh : meshes) {
+        glDrawArrays(GL_TRIANGLES, 0, mesh->vertices.size());
+    }
+
 }
