363f599d7c2e0774c32bff30e834a7d9d239e074
su-ah <emmrald4@gmail.com>
2026-01-18 17:02:34 -0500

    added javadocs

--- include/app/Entity.hpp
+++ include/app/Entity.hpp
@@ -20,17 +20,39 @@ public:
 
 
 	// TODO - TASK: implement the methods below
+
+	/**
+	 * Initializes and adds a component of type <T> with name <name>
+	 *
+	 * @param name Name for the new component
+	 */
 	template <typename T>
 	void add_component(const std::string name);
 
+	/**
+	 * Removes the most recently added component of a specified type
+	 */
 	template <typename T>
 	void remove_component();
 
+	/**
+	 * Removes the most recently added component of a specified name
+	 *
+	 * @param name Name of the component to remove
+	 */
 	void remove_component(const std::string name);
 
+	/**
+	 * Returns a raw pointer to the most recently added component of a specified type
+	 */
 	template <typename T>
 	T* get_component();
 
+	/*
+	 * Returns a raw pointer to the most recently added component of a specified name
+	 *
+	 * @param name Name of the component to find
+	 */
 	template <typename T>
 	T* get_component(const std::string name);
 
