9985c2fb7a37ad4b193af8ec359a4938a15a47fd
Luis <luisf.vbarro@gmail.com>
2026-02-09 19:07:56 -0500

    Added vulkan resources to the texture class, hooray (#170)

--- include/app/ImageUtils.hpp
+++ include/app/ImageUtils.hpp
@@ -66,6 +66,103 @@ struct ImageUtils {
     throw std::runtime_error("Failed to find suitable memory type!");
   }
 
+  static void copyBufferToImage(
+      const sauce::LogicalDevice& logicalDevice,
+      const vk::raii::CommandPool& commandPool,
+      const vk::raii::Queue& queue,
+      const vk::raii::Buffer& srcBuffer,
+      vk::raii::Image& dstImage,
+      uint32_t width,
+      uint32_t height
+  ) {
+    vk::CommandBufferAllocateInfo copyCommandBufferAllocInfo {
+      .commandPool = commandPool,
+      .level = vk::CommandBufferLevel::ePrimary,
+      .commandBufferCount = 1,
+    };
+    vk::raii::CommandBuffer copyCommandBuffer = std::move(logicalDevice->allocateCommandBuffers(copyCommandBufferAllocInfo).front());
+
+    copyCommandBuffer.begin(vk::CommandBufferBeginInfo{ .flags = vk::CommandBufferUsageFlagBits::eOneTimeSubmit });
+
+    vk::BufferImageCopy region {
+      .bufferOffset = 0,
+      .bufferRowLength = 0,
+      .bufferImageHeight = 0,
+      .imageSubresource = {
+        .aspectMask = vk::ImageAspectFlagBits::eColor,
+        .mipLevel = 0,
+        .baseArrayLayer = 0,
+        .layerCount = 1,
+      },
+      .imageOffset = {0, 0, 0},
+      .imageExtent = {width, height, 1},
+    };
+
+    copyCommandBuffer.copyBufferToImage(srcBuffer, dstImage, vk::ImageLayout::eTransferDstOptimal, region);
+    copyCommandBuffer.end();
+
+    queue.submit(vk::SubmitInfo{
+      .commandBufferCount = 1,
+      .pCommandBuffers = &*copyCommandBuffer,
+    }, nullptr);
+    queue.waitIdle();
+  }
+
+  static void transitionImageLayout(
+      const sauce::LogicalDevice& logicalDevice,
+      const vk::raii::CommandPool& commandPool,
+      const vk::raii::Queue& queue,
+      vk::raii::Image& image,
+      vk::ImageLayout oldLayout,
+      vk::ImageLayout newLayout,
+      vk::AccessFlags2 srcAccessMask,
+      vk::AccessFlags2 dstAccessMask,
+      vk::PipelineStageFlags2 srcStageMask,
+      vk::PipelineStageFlags2 dstStageMask
+  ) {
+    vk::CommandBufferAllocateInfo transitionCommandBufferAllocInfo {
+      .commandPool = commandPool,
+      .level = vk::CommandBufferLevel::ePrimary,
+      .commandBufferCount = 1,
+    };
+    vk::raii::CommandBuffer transitionCommandBuffer = std::move(logicalDevice->allocateCommandBuffers(transitionCommandBufferAllocInfo).front());
+
+    transitionCommandBuffer.begin(vk::CommandBufferBeginInfo{ .flags = vk::CommandBufferUsageFlagBits::eOneTimeSubmit });
+
+    vk::ImageMemoryBarrier2 barrier {
+      .srcStageMask = srcStageMask,
+      .srcAccessMask = srcAccessMask,
+      .dstStageMask = dstStageMask,
+      .dstAccessMask = dstAccessMask,
+      .oldLayout = oldLayout,
+      .newLayout = newLayout,
+      .srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED,
+      .dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED,
+      .image = image,
+      .subresourceRange = {
+        .aspectMask = vk::ImageAspectFlagBits::eColor,
+        .baseMipLevel = 0,
+        .levelCount = 1,
+        .baseArrayLayer = 0,
+        .layerCount = 1,
+      },
+    };
+
+    vk::DependencyInfo dependencyInfo {
+      .imageMemoryBarrierCount = 1,
+      .pImageMemoryBarriers = &barrier,
+    };
+
+    transitionCommandBuffer.pipelineBarrier2(dependencyInfo);
+    transitionCommandBuffer.end();
+
+    queue.submit(vk::SubmitInfo{
+      .commandBufferCount = 1,
+      .pCommandBuffers = &*transitionCommandBuffer,
+    }, nullptr);
+    queue.waitIdle();
+  }
+
 };
 
 }
