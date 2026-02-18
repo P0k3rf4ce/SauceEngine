82dfd0a035e616265c1e250eff23b1508bb19468
Damon Ma <damondma@gmail.com>
2026-02-18 13:54:54 -0500

    Cloth Constraint Stuff

--- include/physics/constraints/BendConstraint.hpp
+++ include/physics/constraints/BendConstraint.hpp
@@ -4,12 +4,11 @@
 
 namespace physics {
 
-struct StretchConstraint : public Constraint {
+struct BendConstraint : public Constraint {
 
   virtual void solve(std::vector<Vertex>& vertices, std::vector<glm::vec3>& lagrangeMultipliers, float deltatime) const override;
 
-  float compliance;
-  float restLength;
+  float restAngle;
 
 };
 
